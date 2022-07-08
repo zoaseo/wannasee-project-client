0c915a361cbedb12d1d713a2138f348864017490
7ingout <hadvjin@naver.com>
2022-07-08 17:22:43 +0900

    동영상 해결좀요

---
+++ src/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <video loop autoPlay muted>
+                <source src="bts.mp4" type="video/mp4" alt="dd"/>
+            </video>
+        </div>
+    );
+};
+
+export default MainPage;
