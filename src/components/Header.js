80eab5f9b98ff4cab4de0f48a47ea7bca6df2b0c
7ingout <hadvjin@naver.com>
2022-07-08 17:48:41 +0900

    희재야 파이팅하자

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,43 +3,20 @@ import React from 'react';
 const Header = () => {
     return (
         <div id="header">
+            <nav>
+                <ul>
+                    <li>장르별</li>
+                    <li>지역별</li>
+                    <li>기간별</li>
+                </ul>
+            </nav>
             <div id="top_header">
-                <h1>WANNASEE?</h1>
                 <ul>
                     <li>login</li>
                     <li>join</li>
                     <li>mypage</li>
                 </ul>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        장르별
-                        <ul>
-                            <li>발라드</li>
-                            <li>발라드</li>
-                            <li>발라드</li>
-                            <li>발라드</li>
-                        </ul>
-                    </li>
-                    <li>지역별
-                        <ul>
-                            <li>서울</li>
-                            <li>대학로</li>
-                            <li>홍대</li>
-                            <li>경기</li>
-                        </ul>
-                    </li>
-                    <li>기간별
-                        <ul>
-                            <li>일간</li>
-                            <li>월간</li>
-                            <li>주간</li>
-                            <li>주말</li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
         </div>
     );
 };
--- src/main/index.js
+++ src/main/index.js
@@ -3,8 +3,10 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div>
+            <h2>Welcome</h2>
+            <h1>WANNASEE?</h1>
             <video loop autoPlay muted>
-                <source src="bts.mp4" type="video/mp4" alt="dd"/>
+                <source src="bts.mp4" type="video/mp4"/>
             </video>
         </div>
     );
