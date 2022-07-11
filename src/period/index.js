74842f676a235aa130f8645ac232519fcbacc7c7
7ingout <hadvjin@naver.com>
2022-07-11 17:11:39 +0900

    네비 구현

--- src/period/index.js
+++ src/period/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import './period.css'
 
 const PeriodPage = () => {
     return (
-        <div>
-            
+        <div class="Periodpage">
+        <h1>기간별</h1>
+        <div id="period_div">
+            <ul id="period_ul">
+                <li><Link to='/period'>전체보기</Link></li>
+                <li><Link to='/period'>월간</Link></li>
+                <li><Link to='/period'>주말</Link></li>
+                {/* <li><Link to='/period'></Link></li> */}
+            </ul>
+        </div>
         </div>
     );
 };
