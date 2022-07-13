eb5127b7bed9e477d282e41cdf0d619d71586881
7ingout <hadvjin@naver.com>
2022-07-13 09:31:10 +0900

    detail 구현 ㄱ ㄱ

--- src/genre/genre.js
+++ src/genre/genre.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './genre.css'
-import GenreTd  from './GenreTd';
 
 const GenreComponent = ( {concert} ) => {
-
     return (
-            <li>
-                <img src={concert.imgsrc} alt="singer_pic" />
+            <li id="GenreContent">
+                <span id="span_locaion">{concert.location}</span>
+                <div><img src={concert.imgsrc} alt="singer_pic" /></div>
                 <span>{concert.title}</span>
             </li>
     );
