9115186c06aec845c4f049da358ab77de3f948c7
7ingout <hadvjin@naver.com>
2022-07-22 15:07:50 +0900

    last_dance

--- src/components/MypageComponent.js
+++ src/components/MypageComponent.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+import { API_URL } from '../config/contansts';
+import './MyPage.css'
 
 const MypageComponent = ({data}) => {
+
+     const onDelete = () => {
+        if(window.confirm("정말 삭제하시겠습니까?")){
+            axios.delete(`${API_URL}/delReservation/${data.id}`)
+            .then(result=>{
+                console.log("삭제되었습니다.");
+            })
+            .catch(e=>{
+                console.log(e);
+            })
+        }else{
+            alert("삭제가 취소되었습니다");
+        }
+
+    }
+
     return (
         <tr >
             <td>{data.user_title}</td>
@@ -9,7 +28,7 @@ const MypageComponent = ({data}) => {
             <td>{data.user_date}</td>
             <td>{data.user_start}</td>
             <td>{data.user_num}</td>
-            <td><button>삭제</button></td>
+            <td id="delBtn"><button onClick={onDelete}>삭제</button></td>
         </tr>
     );
 };
